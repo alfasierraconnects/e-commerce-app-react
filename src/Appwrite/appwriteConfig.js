81e384122f050bab1e0ecd52c2e88f21c46110d1
alfasierraconnects <alfasierraconnects@gmail.com>
2024-04-19 20:48:10 +0530

    Updated hostname

--- src/Appwrite/appwriteConfig.js
+++ src/Appwrite/appwriteConfig.js
@@ -1,13 +1,10 @@
 import { Client, Account, Databases } from "appwrite";
 
-const endpoint =
-  process.env.REACT_APP_ENV === "development"
-    ? "http://localhost:3000/v1"
-    : "https://spiffy-elf-00c4f1.netlify.app/v1";
-
 const client = new Client();
 
-client.setEndpoint(endpoint).setProject("661d48ac16d8d3cf0741");
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("661d48ac16d8d3cf0741");
 
 export const account = new Account(client);
 export const databases = new Databases(client);
